dd57a63a3a9c734551f39bd473736c05a53a97b2
wanda azhar <wandaazhar@gmail.com>
2023-09-20 17:04:19 -0400

    setup google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 // import CartProvider from './context/CartContext'
 import SearchProvider from './context/SearchContext'
+import Script from 'next/script'
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({
@@ -27,6 +29,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KN442N46');
+        `}
+      </Script>
+      <GoogleAnalytics GA_MEASUREMENT_ID='G-DH3PG8W6PJ' />
       {/* <CartProvider> */}
       <SearchProvider>
         <body className={inter.className}>
